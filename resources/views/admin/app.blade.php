753e8e5798f3dc20de35df8403eaea5f9303e128
yagosant <yagojf27@gmail.com>
2021-01-13 08:35:14 -0300

    Criando a integração com o Vue

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -11,7 +11,9 @@
 <body class="app sidebar-mini rtl">
     @include('admin.partials.header')
     @include('admin.partials.sidebar')
-    <main class="app-content">
+
+    <main class="app-content" id="app">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         @yield('content')
     </main>
     <script src="{{ asset('backend/js/jquery-3.2.1.min.js') }}"></script>
--- routes/admin.php
+++ routes/admin.php
@@ -33,7 +33,34 @@ Route::group(['prefix'  =>  'admin'], function () {
             Route::get('/{id}/delete', 'Admin\CategoryController@delete')->name('admin.categories.delete');
         
         });
+
+    //Rotas para os atributos    
+        Route::group(['prefix'  =>   'attributes'], function() {
+
+            Route::get('/', 'Admin\AttributeController@index')->name('admin.attributes.index');
+            Route::get('/create', 'Admin\AttributeController@create')->name('admin.attributes.create');
+            Route::post('/store', 'Admin\AttributeController@store')->name('admin.attributes.store');
+            Route::get('/{id}/edit', 'Admin\AttributeController@edit')->name('admin.attributes.edit');
+            Route::post('/update', 'Admin\AttributeController@update')->name('admin.attributes.update');
+            Route::get('/{id}/delete', 'Admin\AttributeController@delete')->name('admin.attributes.delete');
+     
+     //Rotas para os atributos Vue
+            Route::post('/get-values', 'Admin\AttributeValueController@getValues');
+            Route::post('/add-values', 'Admin\AttributeValueController@addValues');
+            Route::post('/update-values', 'Admin\AttributeValueController@updateValues');
+            Route::post('/delete-values', 'Admin\AttributeValueController@deleteValues');
+         });
+
+         Route::group(['prefix'  =>   'brands'], function() {
+
+            Route::get('/', 'Admin\BrandController@index')->name('admin.brands.index');
+            Route::get('/create', 'Admin\BrandController@create')->name('admin.brands.create');
+            Route::post('/store', 'Admin\BrandController@store')->name('admin.brands.store');
+            Route::get('/{id}/edit', 'Admin\BrandController@edit')->name('admin.brands.edit');
+            Route::post('/update', 'Admin\BrandController@update')->name('admin.brands.update');
+            Route::get('/{id}/delete', 'Admin\BrandController@delete')->name('admin.brands.delete');
         
+        });
     });
 
 });
